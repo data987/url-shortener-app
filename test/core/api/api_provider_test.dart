bd2f08a4f0443869eb09412a850615c944d46921
Daniel Tavera <dataanm@gmail.com>
2022-04-26 15:35:25 -0500

    test: :white_check_mark: Test the whole api responses

--- test/core/api/api_provider_test.dart
+++ test/core/api/api_provider_test.dart
@@ -2,10 +2,9 @@ import 'dart:convert';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
-import 'package:mocktail/mocktail.dart';
-import 'package:url_shortener_app/core/api/api_environment.dart';
+import 'package:http/testing.dart';
 import 'package:url_shortener_app/core/api/api_provider.dart';
-import 'package:url_shortener_app/core/api/api_responses.dart';
+import 'package:url_shortener_app/core/api/custom_exceptions.dart';
 
 const Map<String, dynamic> mockResponse = {
   "alias": "51534",
@@ -15,43 +14,77 @@ const Map<String, dynamic> mockResponse = {
   }
 };
 
-class MockHttpClient extends Mock implements http.Client {}
-
-class MockLala extends Mock implements ApiProvider {}
+const String mock400Response = 'Bad request';
 
 void main() {
   late ApiProvider apiProvider;
   late http.Client client;
 
-  setUp(() {
-    client = MockHttpClient();
-    apiProvider = MockLala();
-  });
+  group('Test postUrlToShorten custom responses', () {
+    test('Test postUrlToShorten 200 response', () async {
+      client = MockClient((request) async {
+        return http.Response(json.encode(mockResponse), 200,
+            headers: {'content-type': 'application/json'});
+      });
+      apiProvider = ApiProvider(httpClient: client);
 
-  // test('Test postUrlToShorten response', () async {
-  //   when(
-  //     () => client.post(
-  //       Uri.parse('https://url-shortener-nu.herokuapp.com/api/alias'),
-  //       body: {'url': 'mypage.com'},
-  //       headers: {
-  //         "Accept": "application/json",
-  //         "content-type": "application/json",
-  //       },
-  //     ),
-  //   ).thenAnswer(
-  //     (_) => ApiResponses.apiResponses(
-  //         http.Response(jsonEncode(mockResponse), 200)),
-  //   );
+      expect(await apiProvider.postUrlToShorten('mypage.com'), mockResponse);
+    });
 
-  //   expect(await apiProvider.postUrlToShorten('mypage.com'), mockResponse);
-  // });
+    test('Test postUrlToShorten 400 response', () async {
+      client = MockClient((request) async {
+        return http.Response(mock400Response, 400,
+            headers: {'content-type': 'application/json'});
+      });
+      apiProvider = ApiProvider(httpClient: client);
 
-  test('Test postUrlToShorten response', () async {
-    when(() => apiProvider.postUrlToShorten('mypage.com')).thenAnswer(
-      (_) async => mockResponse,
-    );
+      expect(apiProvider.postUrlToShorten('mypage.com'),
+          throwsA(isA<BadRequestException>()));
+    });
 
-    expect(await apiProvider.postUrlToShorten('mypage.com'), mockResponse);
+    test('Test 401 response', () async {
+      client = MockClient((request) async {
+        return http.Response(mock400Response, 401,
+            headers: {'content-type': 'application/json'});
+      });
+      apiProvider = ApiProvider(httpClient: client);
+
+      expect(apiProvider.postUrlToShorten('mypage.com'),
+          throwsA(isA<UnauthorisedException>()));
+    });
+
+    test('Test 403 response', () async {
+      client = MockClient((request) async {
+        return http.Response(mock400Response, 403,
+            headers: {'content-type': 'application/json'});
+      });
+      apiProvider = ApiProvider(httpClient: client);
+
+      expect(apiProvider.postUrlToShorten('mypage.com'),
+          throwsA(isA<UnauthorisedException>()));
+    });
+
+    test('Test 404 response', () async {
+      client = MockClient((request) async {
+        return http.Response(mock400Response, 404,
+            headers: {'content-type': 'application/json'});
+      });
+      apiProvider = ApiProvider(httpClient: client);
+
+      expect(apiProvider.postUrlToShorten('mypage.com'),
+          throwsA(isA<FetchDataException>()));
+    });
+
+    test('Test 500 response', () async {
+      client = MockClient((request) async {
+        return http.Response(mock400Response, 500,
+            headers: {'content-type': 'application/json'});
+      });
+      apiProvider = ApiProvider(httpClient: client);
+
+      expect(apiProvider.postUrlToShorten('mypage.com'),
+          throwsA(isA<FetchDataException>()));
+    });
   });
 
   tearDown(() {
